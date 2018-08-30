528a349d4c791e315e4c57d65c0557e9f2f8d188
keerthi chintakindi <keerthi.ch26@gmail.com>
2018-08-29 20:33:24 -0400

    updated server.js file

--- server.js
+++ server.js
@@ -101,9 +101,9 @@ var server = app.listen(process.env.PORT || 3000, listen);
 
 // This call back just tells us that the server has started
 function listen() {
-  var host = server.address().address;
+  //var host = server.address().address;
   //const host = '127.0.0.1';
   var port = server.address().port;
-  console.log('Keerthi app listening to http://' +host +':' + port);
+  console.log('Keerthi app listening to http://' +':' + port);
 }
 
